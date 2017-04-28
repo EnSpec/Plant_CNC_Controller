de00007e2fc0f3d2bf41ad1018f601d234f8db29
mwestphall <w.matthew.he@gmail.com>
2017-04-28 03:39:24 -0500

    forms warn for invalid input

--- ardupy_gui/motor_text_gui/js/route_plot.js
+++ ardupy_gui/motor_text_gui/js/route_plot.js
@@ -11,6 +11,36 @@ var update_nodenumbers = function(){
     }
 };
 
+var coord_keydown = function(event){
+   if(event.keyCode == 13)
+       $(this).siblings('.wait').focus(); 
+
+   if((event.key == ' ' && $(this).val().indexOf(' ') != -1) || 
+      (event.key == ',' && $(this).val().indexOf(',') !=-1)){
+       $(this).siblings('.wait').focus(); 
+   } 
+}
+
+var wait_keydown = function(event){
+  if(event.keyCode == 13){
+      var next_node = 1+Number($(this).siblings('.node_num').html());
+      $('.node_num:contains("'+next_node+'")').siblings('.coord').focus();
+      //check that the focus was moved off this
+      if($(this).is(':focus')){
+          append_node();
+          var next_node = 1+Number($(this).siblings('.node_num').html());
+          $('.node_num:contains("'+next_node+'")').siblings('.coord').focus();
+      }
+  }
+};
+
+var form_verify = function(form,regex){
+    if(regex.test(form.val())){
+        form.removeClass('alert-danger');
+    } else {
+        form.addClass('alert-danger');
+    }
+};
 var append_node = function(idx){
     $('#no_nodes').hide();
     var n_nodes = $('.path_node').length+1;
@@ -29,39 +59,22 @@ var append_node = function(idx){
             </div>
            </div>
           </li>`;
+
     if(idx == undefined){
         $('#path_nodes').append(new_node);
     }else{
         $('#path_nodes>li:eq('+idx+')').after(new_node);
     }
+
     $('.node_close').click(function(){
         $(this).closest('.path_node').remove();
         update_nodenumbers();
     });
 
-    $('.coord').keydown(function(event){
-       if(event.keyCode == 13)
-           $(this).siblings('.wait').focus(); 
-
-       if((event.key == ' ' && $(this).val().indexOf(' ') != -1) || 
-          (event.key == ',' && $(this).val().indexOf(',')!=-1)){
-           $(this).siblings('.wait').focus(); 
-       }; 
-    });
-
-    
-    $('.wait').keydown(function(event){
-      if(event.keyCode == 13){
-          var next_node = 1+Number($(this).siblings('.node_num').html());
-          $('.node_num:contains("'+next_node+'")').siblings('.coord').focus();
-          //check that the focus was moved off this
-          if($(this).is(':focus')){
-              append_node();
-              var next_node = 1+Number($(this).siblings('.node_num').html());
-              $('.node_num:contains("'+next_node+'")').siblings('.coord').focus();
-          }
-      }
-    }); 
+    $('.coord').keydown(coord_keydown);
+    $('.coord').change(function(){form_verify($(this),/^\s*,*[0-9]+[,|\s]+[0-9]+\s*,*$/)});
+    $('.wait').keydown(wait_keydown);
+    $('.wait').change(function(){form_verify($(this),/^\s*,*[0-9]+\s*,*$/)});
  
     //focus the first empty coord
     $($('.coord').get().reverse()).each(function(){
